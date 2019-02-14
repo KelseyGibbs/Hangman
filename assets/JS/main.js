9530c60a11b4d1dee459f42b2a6c6202f52112c3
KelseyGibbs <46387160+KelseyGibbs@users.noreply.github.com>
2019-02-14 02:00:10 -0600

    Add files via upload
    Fixed Bugs

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,27 +1,95 @@
-// start game by pressing a random key 
+// Creates main variables
+var guess;
+var letters = [];
+var wrong = 9;
+var word;
+var correctLetters = 0;
+var losses = 0;
+var wins = 0;
 
-// Creates an array of words
-let pokemon = ["Mew", "Pikachu", "Charizard", "Eevee", "Jigglypuff", "Squirtle", "Gyarados", "Snorlax", "Blastoise", "Vulpix", "Dratini", "Growlithe", "Clefairy", "Bulbasaur", "Vaporeon", "Flareon"]
+// Word arry
+var pokemon = [
+    "mew",
+    "pikachu",
+    "charizard",
+    "eevee",
+    "jigglypuff",
+    "squirtle",
+    "gyarados",
+    "snorlax",
+    "blastoise",
+    "vulpix",
+    "dratini",
+    "growlithe",
+    "clefairy",
+    "bulbasaur",
+    "vaporeon",
+    "flareon"
+];
 
-// Selects a random word from the array
-var randomword = Math.floor(Math.random() * pokemon.length); 
-
-// Console logs that word, because YAY 
-console.log(pokemon[randomword])
-
-// display underscores that are === to the length of the randomly selected words
-
-var userImput = [];
-for (var i = 0; i < randomword.length; i++) {
-userImput[i] = "_";
+// Chooses a random word from the pokemon array 
+function randomName() {
+    document.getElementById(`wordDisplay`).innerHTML = " ";
+    return pokemon[Math.floor(Math.random() * pokemon.length)];
 }
-var unguessed = randomword.length;
 
-console.log(userImput[i])
-
-while (unguessed > 0) {
-
+// Creating a new function to 
+function getStarted() {
+    // Creating a variable and assigning it to the return of the function randomName
+    word = randomName();
+    // Creates Underscores equal to the word
+    for (i = 0; i < word.length; i++) {
+        letters[i] = "_";
+    }
+    // Displays Underscores
+    document.getElementById("wordDisplay").innerHTML = letters.join(" ");
+    console.log(letters)
+    console.log(word);
 }
 
-// create if else statements to compare the user imput to the randomly selected word & change the display from an underscore to the correct letter
+// Main game
+function main() {
+    document.onkeyup = function (event) {
 
+
+
+
+        guess = (String.fromCharCode(event.keyCode)).toLowerCase();
+
+        document.getElementById(`guessed`).append(guess);
+
+        for (i = 0; i < word.length; i++) {
+
+            if (guess === word[i]) {
+                correctLetters++;
+                console.log(correctLetters)
+                letters[i] = guess;
+                document.getElementById("wordDisplay")
+                    .innerHTML = letters.join(" ");
+            }
+
+            if (correctLetters === word.length) {
+                console.log("fuck yea")
+                correctLetters = 0;
+                wins++;
+
+                document.getElementById(`wins`).innerHTML = ("Wins " + wins);
+
+                reset();
+                
+            }
+
+        }
+    };
+}
+
+function reset() {
+    document.getElementById(`guessed`).innerHTML = " ";
+    document.getElementById(`wordDisplay`).innerHTML = " ";
+    letters = [];
+    // randomName();
+    getStarted();
+    main();
+}
+getStarted();
+main();
